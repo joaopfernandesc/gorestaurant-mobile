6de9db004f0340672e9745fac50a8bb566bfe661
João <joaopfernandesc@gmail.com>
2020-10-31 23:42:09 -0300

    Finished

--- src/pages/Orders/index.tsx
+++ src/pages/Orders/index.tsx
@@ -32,7 +32,16 @@ const Orders: React.FC = () => {
 
   useEffect(() => {
     async function loadOrders(): Promise<void> {
-      // Load orders from API
+      const { data } = await api.get('/orders')
+
+      const orders = data.map((order: Food) => {
+        return {
+          ...order,
+          formattedPrice: formatValue(order.price),
+        }
+      })
+
+      setOrders(orders)
     }
 
     loadOrders();
